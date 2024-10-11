4fb9a15c1fc004f073dfaa31c6b15e4b60e2adfd
saadsgh123 <sdsghouri@gmail.com>
2024-10-11 14:05:26 +0100

    update README

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that contains a single function that concatenate two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate two strings"""
+    return "{0}{1}".format(str1, str2)
