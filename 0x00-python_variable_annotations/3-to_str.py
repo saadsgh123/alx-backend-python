5eab2a2769bd53c451fe606234c21b501b8e6cc2
saadsgh123 <sdsghouri@gmail.com>
2024-10-11 14:53:09 +0100

    task 3 try 1

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+"""Module that contains a single function that convert float to string"""
+
+
+def to_str(n: float) -> str:
+    """
+        Convert float to str
+
+        :param n: float number
+        :return: n as string.
+        """
+    return "{}".format(n)
