3cf367a5262cf08dedcdf78fe8641e297fa6b597
saadsgh123 <sdsghouri@gmail.com>
2024-10-11 14:13:05 +0100

    task 2 try 1

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""module that contains single function that takes a float and return its floor"""
+
+
+def floor(n: float) -> int:
+    """
+    Convert float to integer
+    :arg
+        n (float): The float variable
+    :return
+        int: the floor of the float
+    """
+    return int(n)
