2c26926867b2b93f711d16302ee779650d11632b
saadsgh123 <sdsghouri@gmail.com>
2024-10-11 15:59:06 +0100

    task 6 try 3 (list type issue)

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """function which takes a list of int and floats and returns sum as a float."""
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: Union[float, int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """
     Sum of mixed type list (int & float)
     :param mxd_lst: list of integer and float
@@ -13,3 +13,11 @@ def sum_mixed_list(mxd_lst: Union[float, int]) -> float:
     for e in mxd_lst:
         result += e
     return result
+
+
+if __name__ == '__main__':
+    print(sum_mixed_list.__annotations__)
+    mixed = [5, 4, 3.14, 666, 0.99]
+    ans = sum_mixed_list(mixed)
+    print(ans == sum(mixed))
+    print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
