f40282111c6b31ae4a036ccbf958c90984a379a7
saadsgh123 <sdsghouri@gmail.com>
2024-10-11 15:59:36 +0100

    task 6 try 3 (list type issue)

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -13,11 +13,3 @@ def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     for e in mxd_lst:
         result += e
     return result
-
-
-if __name__ == '__main__':
-    print(sum_mixed_list.__annotations__)
-    mixed = [5, 4, 3.14, 666, 0.99]
-    ans = sum_mixed_list(mixed)
-    print(ans == sum(mixed))
-    print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
