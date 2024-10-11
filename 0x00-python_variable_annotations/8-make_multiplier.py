5a3883b8c672d9fddf988683b52fd83fd27d897a
saadsgh123 <sdsghouri@gmail.com>
2024-10-11 17:01:21 +0100

    task 8 try 1

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+module that contain a single function that takes a
+float multiplier as argument and
+returns a function that multiplies a float by multiplier.
+"""
+from collections.abc import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    function that multiplies a float by multiplier.
+    :param multiplier: float
+    :return: a callable function
+    """
+    def square(n: float) -> float:
+        return multiplier * n
+
+    return square
