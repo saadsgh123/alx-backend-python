12462c77a1462da633bbad1f60c48b5f1397dcde
saadsgh123 <sdsghouri@gmail.com>
2024-10-14 16:11:36 +0100

    done

--- 0x01-python_async_function/main.py
+++ 0x01-python_async_function/main.py
@@ -1,9 +1,21 @@
-#!/usr/bin/env python3
-
 import asyncio
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
+async def download_file1():
+    print("Downloading file 1...")
+    await asyncio.sleep(1)  # Simulate waiting for download
+    print("Downloaded file 1")
+
+
+async def download_file2():
+    print("Downloading file 2...")
+    await asyncio.sleep(1)  # Simulate waiting for download
+    print("Downloaded file 2")
+
+
+async def main():
+    await asyncio.gather(download_file2(), download_file1())  # Run both downloads concurrently
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
