beeb02ee9b11a94ea4a1ddf56725477316d58921
saadsgh123 <sdsghouri@gmail.com>
2024-10-15 18:26:57 +0100

    task 2 try 1 🥶

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""a python module to measure the execution time"""
+import asyncio
+from time import perf_counter
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    measure_runtime - function execute async_com 4 times
+    :return: the total execution time required to complete the task
+    """
+    start = perf_counter()
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    elapsed = perf_counter() - start
+    return elapsed
