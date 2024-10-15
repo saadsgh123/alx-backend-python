d130dd4aa83afc274190dad548218ec347a757ed
saadsgh123 <sdsghouri@gmail.com>
2024-10-15 18:04:08 +0100

    task 0 try 4 😅

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import asyncio
 from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     async_generator - function to loop 10 times
     Arguments:
